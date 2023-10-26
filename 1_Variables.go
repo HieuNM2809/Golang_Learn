5c4171244a1b82b4c766fa938e1872fc341f0a1b
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2023-10-26 17:22:22 +0700

    1_Variables.go

---
+++ 1_Variables.go
@@ -0,0 +1,76 @@
+/*
+	1. Định nghĩa biến trong ngôn ngữ lập trình
+	2. Cú pháp khai báo biến
+	3. Global and block scope
+	4. Shadow
+	5. Declared and not used
+	6. Export global scope
+	7. Naming convention
+	8. Convert type
+*/
+
+//---------------------------------------------------------
+// 1. Định nghĩa biến trong ngôn ngữ lập trình
+// 2. Cú pháp khai báo biến
+
+// Khai báo 1 biến 
+//(1) var i int           -- Khai báo 
+//(2) var i int = 10      -- Khai báo và khởi tạo
+//(3) i := 10             -- Kháo báo và tự ép kiểu 
+
+
+// package main
+
+// import "fmt"
+
+// var j int = 1 
+
+// func main() {
+// 	i := 1
+// 	var j int = 2
+// 	j := 2
+// 	fmt.Println("Hello, 世界", i+1, j)
+// }
+
+//-----------------------------------------------------
+// 3. Global and block scope
+// 4. Shadow  - Global được khai báo lại trong func
+
+// package main
+
+// import "fmt"
+
+// var j int = 1 
+// var (
+// 	a int = 1
+// 	b string = "1"
+// 	c int = 1
+// )
+
+// func main() {
+// 	fmt.Println("Hello, 世界", a,b,c,j)
+// }
+
+//----------------------------------------------------
+// 6. Export global scope
+// var Num int = 10
+// -> Viết hoa chữ cái đầu đề các pakeage khác truy suất được
+
+//----------------------------------------------------
+// 7. Naming convention
+// -> camel
+
+//----------------------------------------------------
+// 8. Convert type
+// hoặc sử dụng strconv
+package main
+
+import "fmt"
+
+
+func main() {
+	var a int  = 100
+	var b float32  = float32(a)
+	fmt.Printf("%v %T", b, b)
+}
+
