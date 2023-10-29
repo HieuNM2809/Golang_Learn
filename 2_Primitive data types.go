ed976dbe70367c12853c60e1319c461d8eb8a47a
HieuNM <nmhieucoder@gmail.com>
2023-10-29 10:44:07 +0700

    2_Primitive Data Types

---
+++ 2_Primitive data types.go
@@ -0,0 +1,24 @@
+//1. Kiểu Dữ Liệu Nguyên Thủy (Primitive Data Types)
+//    1.1 Kiểu Boolean
+//    1.2 Kiểu Số Học (Numeric Types)
+//        - Số Nguyên (Integer Types)
+//            - Số Nguyên có dấu (Signed Integers)
+//                - int8
+//                - int16
+//                - int32
+//                - int64
+//            - Số Nguyên không dấu (Unsigned Integers)
+//                - uint8
+//                - uint16
+//                - uint32
+//                - uint64
+//        - Số Thực (Floating-Point Types)
+//            - float32
+//            - float64
+//        - Số Ảo (Complex Types)
+//            - complex64
+//            - complex128
+//    1.3 Kiểu Văn Bản (Text Types)
+//        - Chuỗi (String)
+//        - Mảng Byte (Byte)
+//        - Rune
