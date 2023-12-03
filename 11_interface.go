bc7572b3b6223a1a80b7594953cb493ed368b6e9
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2023-12-03 14:49:52 +0700

    11_interface.go

---
+++ 11_interface.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello World"))
+}
+
+type Writer interface {
+	Write([]byte) (int, error)
+}
+
+type ConsoleWriter struct{}
+
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
+}
